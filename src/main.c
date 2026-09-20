ba66a1022781cb899c4779e046d09c34d81051c3
Takayuki Miyahara <voyager.3taka28@gmail.com>
2026-09-20 19:37:09 +0900

    feat: tcp echo server

--- src/main.c
+++ src/main.c
@@ -30,22 +30,24 @@ int main() {
         perror("listen error");
     }
 
-    // accept
-    int client_fd = accept(sock, NULL, NULL);
+    for (;;) {
+        // accept
+        int client_fd = accept(sock, NULL, NULL);
 
-    // recv
-    char buf[1024];
-    int num_received = recv(client_fd, buf, sizeof(buf), 0);
-    printf("%d byte received\n", num_received);
-    // TODO error handle
+        // recv
+        char buf[1024];
+        int num_received = recv(client_fd, buf, sizeof(buf), 0);
+        printf("%d byte received\n", num_received);
+        // TODO error handle
 
-    // send
-    send(client_fd, buf, num_received, 0);
-    // TODO error handle
+        // send
+        send(client_fd, buf, num_received, 0);
+        // TODO error handle
 
-    // close
-    close(client_fd);
-    // TODO error handle
+        // close
+        close(client_fd);
+        // TODO error handle
+    }
 
     close(sock);
     // TODO error handle
