84f27b7e98e8e007c476249e5d478189b9e89e8b
Takayuki Miyahara <voyager.3taka28@gmail.com>
2026-09-20 20:08:14 +0900

    refactor: src/tcp_server.c

--- src/tcp_server.c
+++ src/tcp_server.c
@@ -6,21 +6,27 @@
 
 int create_tcp_server() {
     int sock = socket(PF_INET, SOCK_STREAM, 0);
-    // TODO error handle
+    if (sock == -1) {
+        perror("socket error");
+        return -1;
+    }
 
     int option_on = 1;
-    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
-                &option_on, sizeof(option_on)) == -1) {
+    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &option_on, sizeof(option_on)) == -1) {
         perror("setsockopt error");
+        close(sock);
+        return -1;
     }
+
     struct sockaddr_in addr = {
         .sin_family = AF_INET,
         .sin_port = htons(8080),
         .sin_addr.s_addr = INADDR_ANY,
     };
-
     if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
         perror("bind error");
+        close(sock);
+        return -1;
     }
     return sock;
 }
